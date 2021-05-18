a512bbde4afa03ada5c632e8336a1a4fe22d2e42
Arun Sharma <sharma.arun333@gmail.com>
2021-05-19 04:01:59 +0530

    added analytics

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -48,14 +48,26 @@ function DefaultLayout({
         })();
         `}
         </script> */}
+
+        <script type="text/javascript">
+          {`var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//ganalytics.exclamationx.com/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '2']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();`}
+        </script>
       </HelmetContainer>
       <div>
         <Header safeAreaPadding="0px" />
         <SingleSignOn />
 
-        <div  className="min-h-screen">
-          {children}
-        </div>
+        <div className="min-h-screen">{children}</div>
         <Footer />
       </div>
     </>
